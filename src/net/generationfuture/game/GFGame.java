66dbf84391be5a5ff102b52b7511996f8ebc9db1
Martin Giger <martin.a.giger@sunrise.ch>
2011-11-16 20:15:18 +0100

    + Minimap 9+ Start of coordinate system for moving

--- src/net/generationfuture/game/GFGame.java
+++ src/net/generationfuture/game/GFGame.java
@@ -6,7 +6,13 @@ import org.newdawn.slick.*;
 public class GFGame extends BasicGame{
 
     // @param args
-     
+    Image minimap = null;
+    int x = 0;
+    int y = 0;
+    
+    int minimapx = 248;
+    int minimapy = 200;
+    
     public GFGame()
     {
         super("GFGame");
@@ -15,8 +21,8 @@ public class GFGame extends BasicGame{
     @Override
     public void init(GameContainer gc) 
 			throws SlickException {
- 
-    }
+        minimap = new Image("materials/mystery.jpg");
+     }
  
     @Override
     public void update(GameContainer gc, int delta) 
@@ -28,12 +34,13 @@ public class GFGame extends BasicGame{
     public void render(GameContainer gc, Graphics g) 
 			throws SlickException 
     {
- 
+        minimap.draw(0, 0);
     }
  
     public static void main(String[] args) 
 			throws SlickException
     {
+         
          AppGameContainer app = new AppGameContainer(new GFGame());
  
          app.setDisplayMode(800, 600, false);
