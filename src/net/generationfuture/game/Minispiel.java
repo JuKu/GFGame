50ddcabe50cbd8b8922534ce2bffa067a9072369
JuKu <JustinKuenzel@aol.com>
2011-12-30 22:24:37 +0100

    test

---
+++ src/net/generationfuture/game/Minispiel.java
@@ -0,0 +1,35 @@
+package net.generationfuture.game;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.*;
+
+public abstract class Minispiel {
+    
+    protected Image background;
+    protected Image mouse;
+    protected int x;
+    protected int y;
+    protected int mouse_x = 10;
+    protected int mouse_y = 10;
+    protected Boolean enable_mouse = true;
+    protected Boolean enable_keys = true;
+    protected Boolean is_background = true;
+    protected Boolean is_mouse = true;
+    
+    public void paint (Graphics g) {
+        
+        if (is_background) {
+            background.draw(1, 1);
+        }
+        
+        if (is_mouse) {
+            mouse.draw(mouse_x, mouse_y);
+        }
+        
+    }
+    
+    public void enableMouse (Boolean enableMouse) {
+        this.enable_mouse = enableMouse;
+    }
+    
+}
