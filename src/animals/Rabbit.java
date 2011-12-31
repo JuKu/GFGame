76919ee08f7215b60f6cc073ea350b3bdd286763
JuKu <JustinKuenzel@aol.com>
2011-12-31 13:36:10 +0100

    Dateien hinzugefügt.

---
+++ src/animals/Rabbit.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animals;
+
+/**
+ *
+ * @author Justin
+ */
+public class Rabbit {
+    
+}
