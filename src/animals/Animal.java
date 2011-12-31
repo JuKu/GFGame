ca78593f4f8711adc44fc551d781600214fd6c75
JuKu <JustinKuenzel@aol.com>
2011-12-31 13:44:43 +0100

    Grafiken "Baum" hinzugefügt.

---
+++ src/animals/Animal.java
@@ -0,0 +1,12 @@
+package animals;
+
+import org.newdawn.slick.Image;
+
+public abstract class Animal {
+    
+    Image walking_left1;
+    Image walking_left2;
+    Image walking_right1;
+    Image walking_right2;
+    
+}
--- src/animals/Rabbit.java
+++ src/animals/Rabbit.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package animals;
 
-/**
- *
- * @author Justin
- */
-public class Rabbit {
+public class Rabbit extends Animal {
     
 }
