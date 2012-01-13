5dbbd543157cfc5c9549da822cb8168241ae7c3f
JuKu <JustinKuenzel@aol.com>
2012-01-13 16:56:11 +0100

    Dateien hinzugefügt.

--- src/net/generationfuture/game/GameMenu.java
+++ src/net/generationfuture/game/GameMenu.java
@@ -1,5 +1,38 @@
 package net.generationfuture.game;
 
+import org.newdawn.slick.Image;
+
 public class GameMenu {
+    
+    private String menu_title = "";
+    private GameMenuItem menuItems[];
+    private int MenuCounter = 0;
+    private int MenuItemCounter = 0;
+    
+    private Boolean isShown = false;
+    private Image menu_image;
+    
+    public GameMenu (String menu_title) {
+        this.menu_title = menu_title;
+        menuItems = new GameMenuItem[10];
+    }
+    
+    public void setVisible (Boolean visible) {
+        this.isShown = visible;
+    }
+    
+    public void addMenuItem (GameMenuItem menuItem) {
+        menuItems[MenuItemCounter] = menuItem;
+        MenuItemCounter++;
+    }
+    
+    public void removeAll () {
+        
+        for (int i = 0; i <= 10; i++) {
+            menuItems[i] = null;
+            MenuItemCounter = 0;
+        }
+        
+    }
     
 }
